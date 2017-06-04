258092d0e16a40756063d9eb73c4d59e39f5d4c0
ricardo <ledo.tulio@gmail.com>
2017-06-03 22:42:02 -0300

    cards: create card resource

--- dist/resources/index.js
+++ dist/resources/index.js
@@ -8,9 +8,14 @@ var _boards = require('./boards');
 
 var _boards2 = _interopRequireDefault(_boards);
 
+var _cards = require('./cards');
+
+var _cards2 = _interopRequireDefault(_cards);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = {
-  boards: _boards2.default
+  boards: _boards2.default,
+  cards: _cards2.default
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL2xpYi9yZXNvdXJjZXMvaW5kZXguanMiXSwibmFtZXMiOlsiYm9hcmRzIl0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQTs7Ozs7O2tCQUVlO0FBQ2JBO0FBRGEsQyIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBib2FyZHMgZnJvbSAnLi9ib2FyZHMnXG5cbmV4cG9ydCBkZWZhdWx0IHtcbiAgYm9hcmRzXG59XG4iXX0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL2xpYi9yZXNvdXJjZXMvaW5kZXguanMiXSwibmFtZXMiOlsiYm9hcmRzIiwiY2FyZHMiXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBOzs7O0FBQ0E7Ozs7OztrQkFFZTtBQUNiQSwwQkFEYTtBQUViQztBQUZhLEMiLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgYm9hcmRzIGZyb20gJy4vYm9hcmRzJ1xuaW1wb3J0IGNhcmRzIGZyb20gJy4vY2FyZHMnXG5cbmV4cG9ydCBkZWZhdWx0IHtcbiAgYm9hcmRzLFxuICBjYXJkcyxcbn1cbiJdfQ==
